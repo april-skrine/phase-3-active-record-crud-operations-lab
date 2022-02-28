8fa1b41c2ad7221ca0b08c40e6898f6ee401805e
April Skrine <april.skrine@gmail.com>
2022-02-28 15:38:55 -0600

    commit tree 0b626504830f810cec76e6e87007187342bf9e1b

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,11 @@
 class Movie < ActiveRecord::Base
 
+    def self.create_with_title some_title
+        self.create(title: some_title)
+    end
+
+    def self.first_movie
+        self.first
+    end
+
 end
